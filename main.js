b8b58664a4e20474360e958609abadd9f71de056
nomon9899 <nomon67211998gmail.com>
2021-07-12 13:18:56 +0500

    Firs commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// // let  boxer = "Bakhadir Jalalov";
+// // let position = 4;
+// // let medal ;
+// //
+// //
+// // if (position == 1){
+// //     medal = "oltin";
+// // }
+// // else if (position == 2){
+// //     medal = "kumush";
+// // }
+// // else if (position == 3){
+// //     medal = "bronza";
+// // }
+// // else {
+// //     medal = "sholpinchaq"
+// // }
+// //     console.log(boxer  + " " +medal + " medal oldi")
+//
+// let number = 1+2;
+// switch (number){
+//     case 1:
+//     console.log("kichik");
+//         break;
+//     case 2:
+//     console.log("Kichikroq");
+//         break;
+//     case 3:
+//     console.log("Aynan siz kutgan qiymat");
+//         break;
+//     default:
+//         console.log("Mavjud bo'lmagan qiymat");
+// }
+
+let single = 'simgle-quoted';
+let double =  "double quoted";
+
+let backticks = 'backticks';
+
+// console.log(typeof single);
+// console.log(typeof double);
+// console.log(typeof backticks);
+
+function sum(numberOne, nomberTwo){
+    return numberOne + nomberTwo;
+}
+
+// console.log('20 + 30 = ${sum (20, 30)}');
+
+let programmingLanguage = 'C/C++
+    *Java
+    *Python
+    *C#';
+console.log(programmingLanguage);
